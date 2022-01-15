901bd25d4160370d859785c4884a0d4440a07059
lejt <92820402+lejt@users.noreply.github.com>
2022-01-14 17:20:59 -0800

    testing customer user

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+class User(AbstractUser):
+    is_member = models.BooleanField('member status', default=False)
+    is_staff = models.BooleanField('staff status', default=False)
+
+class Member(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    name = models.CharField(max_length=20)

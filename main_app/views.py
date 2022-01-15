8eca133b948bd459414ef3ee542ffb49cd1753e6
lejt <92820402+lejt@users.noreply.github.com>
2022-01-14 17:56:00 -0800

    sign up page add with no forms yet

--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
 def signup(request):
+
     return render(request, 'registration/signup.html')
 
 def member_signup(request):

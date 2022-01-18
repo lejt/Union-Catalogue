13cc61b8e13177e7a59621180136a9515a500424
lejt <92820402+lejt@users.noreply.github.com>
2022-01-17 22:00:27 -0800

    clubs, book list working

--- main_app/models.py
+++ main_app/models.py
@@ -5,8 +5,6 @@ from django.urls import reverse
 
 # General User login
 class User(AbstractUser):
-    # is_lib_member = models.BooleanField('is member', default=False)
-    # is_lib_staff = models.BooleanField('is staff', default=False)
     TYPE_CHOICES = (
         ('M', 'Member'),
         ('S', 'Staff'),
@@ -16,15 +14,13 @@ class User(AbstractUser):
     def __str__(self):
         return f"{self.get_user_type_display()}"
 
-class Books(models.Model):
+class Book(models.Model):
     isbn = models.IntegerField()
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=50)
     
     def get_absolute_url(self):
-        return reverse('detail', kwargs={'books_id': self.id})
-    
-    
+        return reverse('books_detail', kwargs={'books_id': self.id})
     
 
 # User split into either Member or Staff --------------------------------
@@ -43,7 +39,7 @@ class Staff(models.Model):
         return self.user_staff.first_name
 # ------------------------------------------------------------------------
 
-# A club has many members
+# Many club has many members - Club model holds key of Members
 class Club(models.Model):
     ROOMS = (
         ('a', '1st Floor Auditorium'),
--- main_app/urls.py
+++ main_app/urls.py
@@ -5,20 +5,20 @@ urlpatterns = [
     # view functions
     path('', views.home, name='home'),
     path('members/', views.members_index, name='members_index'),
-    path('members/<int:member_id>/', views.members_detail, name='members_detail'),
     path('books/', views.books_index, name="books"),
     path('books/<int:books_id>/', views.books_detail, name="books_detail"),
+    path('books/create/', views.BookCreate.as_view(), name='book_create'),
+
+    # member + staff details page here - WIP 
+    path('members/<int:member_id>/', views.members_detail, name='members_detail'),
     path('staffs/<int:staff_id>/', views.staffs_detail, name='staffs_detail'),
     
     path('clubs/', views.clubs_index, name='clubs_index'),
     path('clubs/add_club/', views.add_club, name='add_club'),
 
-    # CBV
-    # path('clubs/create/', views.ClubCreate.as_view(), name='clubs_create'),
-
-
+    # --------------------------------------------------------------------
     # no login path needed, already built-in
     path('accounts/signup/', views.signup, name='signup'),
-    path('books/create/', views.BookCreate.as_view(), name='book_create'),
+    # --------------------------------------------------------------------
     
 ]
